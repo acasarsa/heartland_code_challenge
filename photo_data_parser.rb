2644c990c5ef39ab239df39bafd44ee80d750e9b
Andrew Casarsa <acasarsa@gmail.com>
2023-12-04 17:56:22 -0500

    Refactoring for Readability and Efficiency
    - Modularize the parsing logic into distinct methods for clarity.
    - Implement calculate_group_sizes and rename_filenames as private class methods for better encapsulation.
    - Optimize the process of renaming photo filenames by pre-calculating group sizes.
    - Add list_reformatted_filenames method for easy retrieval of updated filenames.

--- photo_data_parser.rb
+++ photo_data_parser.rb
@@ -1,40 +1,47 @@
+# frozen_string_literal: false
+
 require 'date'
 require 'byebug'
 
 # parse photo data and return new filenames in original order separated by a newline
 class PhotoDataParser
   def self.parse(data)
-    photo_info = data.split("\n")
-
-    photos = []
-
-    photo_info.each do |str|
-      filename, city, timestamp = str.split(', ')
-      photos << { filename:, city:, timestamp: DateTime.parse(timestamp) }
+    parsed_photos = data.split("\n").map do |photo_info|
+      filename, city, timestamp = photo_info.split(', ')
+      { filename:, city:, timestamp: DateTime.parse(timestamp) }
     end
 
-    grouped_by_city = photos.group_by { |photo| photo[:city] }
+    photos_grouped_by_city = parsed_photos.group_by { |photo| photo[:city] }
+    city_group_sizes = calculate_group_sizes(photos_grouped_by_city)
 
-    formatted_photo_str = ''
-    grouped_by_city.each do |city, city_photos|
-      group_size = city_photos.size.to_s.size
-
+    photos_grouped_by_city.each_value do |city_photos|
       sorted_photos = city_photos.sort_by { |photo| photo[:timestamp] }
-
-      sorted_photos.each_with_index do |photo, index|
-        city = photo[:city]
-        file_type = File.extname(photo[:filename])
-        count = index + 1
-        count_sufix = count.to_s.rjust(group_size, '0')
-        photo[:filename] = "#{city}#{count_sufix}#{file_type}"
-      end
+      rename_filenames(sorted_photos, city_group_sizes)
     end
-
-    photos.each do |photo|
-      formatted_photo_str << "#{photo[:filename]}\n"
-    end
-    formatted_photo_str
+    parsed_photos
   end
+
+  def self.list_reformated_filenames(data)
+    parsed_photos = parse(data)
+    parsed_photos.map { |photo| photo[:filename] }.join("\n")
+  end
+
+  def self.rename_filenames(sorted_photos, city_group_sizes)
+    sorted_photos.each_with_index do |photo, index|
+      group_size = city_group_sizes[photo[:city]]
+      count_sufix = (index + 1).to_s.rjust(group_size, '0')
+      file_type = File.extname(photo[:filename])
+
+      photo[:filename] = "#{photo[:city]}#{count_sufix}#{file_type}"
+    end
+  end
+  private_class_method :rename_filenames
+
+  # returns a hash
+  def self.calculate_group_sizes(photos_gouped_by_city)
+    photos_gouped_by_city.transform_values { |group| group.size.to_s.size }
+  end
+  private_class_method :calculate_group_sizes
 end
 
 sample_data = "photo.jpg, Krakow, 2013-09-05 14:08:15
@@ -53,5 +60,4 @@ e.png, Krakow, 2016-01-02 09:49:09
 f.png, Krakow, 2016-01-02 10:55:32
 g.jpg, Krakow, 2016-02-29 22:13:11"
 
-puts PhotoDataParser.parse(sample_data)
-
+puts PhotoDataParser.list_reformated_filenames(sample_data)
