fe12101ba457cbe43dfb9c063328880aebe8024c
Andrew Casarsa <acasarsa@gmail.com>
2023-12-04 18:20:03 -0500

    Add Documentation
    - method descriptions with types for params and return
    - add class description

--- photo_data_parser.rb
+++ photo_data_parser.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: false
 
 require 'date'
-require 'byebug'
 
-# parse photo data and return new filenames in original order separated by a newline
+# PhotoDataParser is a stateless utility class responsible for parsing a string of photo data.
+# It provides functionality to reformat filenames based on city and timestamp,
+# and can output a list of reformatted filenames. This class is designed to
+# handle the grouping of photos by city, renaming files for uniformity, and
+# maintaining the original order of the photos.
 class PhotoDataParser
+  # Parses the raw photo data string and returns an array of photo hashes.
+  # @param data [String] the raw photo data as a newline-separated string.
+  # @return [Array<Hash>] an array of photo hashes with filename, city, and timestamp.
   def self.parse(data)
     parsed_photos = data.split("\n").map do |photo_info|
       filename, city, timestamp = photo_info.split(', ')
@@ -21,11 +27,18 @@ class PhotoDataParser
     parsed_photos
   end
 
+  # Generates a string of reformatted filenames, each on a new line.
+  # @param data [String] the raw photo data as a newline-separated string.
+  # @return [String] a string of reformatted filenames, separated by newlines.
   def self.list_reformated_filenames(data)
     parsed_photos = parse(data)
     parsed_photos.map { |photo| photo[:filename] }.join("\n")
   end
 
+  # Renames filenames in a sorted array of photo hashes, based on city group sizes.
+  # @param sorted_photos [Array<Hash>] sorted array of photo hashes.
+  # @param city_group_sizes [Hash] hash mapping city names to group size digits.
+  # @return [void]
   def self.rename_filenames(sorted_photos, city_group_sizes)
     sorted_photos.each_with_index do |photo, index|
       group_size = city_group_sizes[photo[:city]]
@@ -37,7 +50,9 @@ class PhotoDataParser
   end
   private_class_method :rename_filenames
 
-  # returns a hash
+  # Calculates the group sizes for each city in the photo collection.
+  # @param photos_gouped_by_city [Hash] photos grouped by city.
+  # @return [Hash] hash mapping each city to the size of its photo group.
   def self.calculate_group_sizes(photos_gouped_by_city)
     photos_gouped_by_city.transform_values { |group| group.size.to_s.size }
   end
